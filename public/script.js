28b36955325188a4c423758f68dc8724fa7564aa
Pareek Ravi <pareek96@gmail.com>
2017-05-06 16:45:54 -0400

    Merging

--- public/script.js
+++ public/script.js
@@ -12,7 +12,7 @@
  var allCards = [];
 
  function getCards(){
- 	var ref = database.ref();
+ 	var ref = database.ref('words');
  	ref.once('value').then(function(snapshot){
  		snapshot.forEach(function(childSnapshot){
  			allCards.push(childSnapshot.val())
@@ -21,7 +21,7 @@
  }
 
  function fillCard(){
-= 	 	var cardNum = Math.floor(Math.random() * allCards.length);
+  	 	var cardNum = Math.floor(Math.random() * allCards.length);
  	 	// console.log(cardNum);
  	 	var card = allCards[cardNum];
  	 	document.getElementById('wordText').innerText = card.word;
@@ -30,56 +30,48 @@
  	 	document.getElementById('noSay3Text').innerText = card.noSay3;
  	 	document.getElementById('noSay4Text').innerText = card.noSay4;
  	 	document.getElementById('noSay5Text').innerText = card.noSay5;
- 	
+
  	 	allCards.splice(cardNum,1);
  	 	
  	 	if (allCards.length==0){
- 		getCards();
- 		console.log('getting new cards');
- 		// console.log(allCards);
- 	}
- }
-=	var cardNum = Math.floor(Math.random() * allCards.length);
- 	// console.log(cardNum);
- 	var card = allCards[cardNum];
- 	document.getElementById('wordText').innerText = card.word;
- 	document.getElementById('noSay1Text').innerText = card.noSay1;
- 	document.getElementById('noSay2Text').innerText = card.noSay2;
- 	document.getElementById('noSay3Text').innerText = card.noSay3;
- 	document.getElementById('noSay4Text').innerText = card.noSay4;
- 	document.getElementById('noSay5Text').innerText = card.noSay5;
-
- 	allCards.splice(cardNum,1);
- 	
- 	if (allCards.length==0){
- 		getCards();
- 		console.log('getting new cards');
- 	}
+ 	 		getCards();
+ 	 		console.log('getting new cards');
+ 			// console.log(allCards);
+ 		}
  }
 
-function startRound(){
-	var countDownDate = new Date().getTime() + 61000;
-	fillCard()
-	var x = setInterval(function(){
+ function startRound(){
+ 	var countDownDate = new Date().getTime() + 61000;
+ 	fillCard()
+ 	var x = setInterval(function(){
 		// Get todays date and time
 		var now = new Date().getTime();	
 		// Find the distance between now an the count down date
 		var distance = countDownDate - now;
 		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-		// Output the result in an element with id="demo"
 		document.getElementById("timer").innerHTML = 	 seconds + "s ";
 
 		if (seconds <= 0){
 			clearInterval(x);
 		}
 	},1000);
-}
+ }
 
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-    var countDownDate = new Date().getTime() + 3510;
+ function openNav() {
+ 	document.getElementById("myNav").style.height = "100%";
+ 	initApp();
+ 	getCards();
+ }
 
-	var x = setInterval(function(){
+ function closeNav() {
+ 	startRound();
+ 	document.getElementById("myNav").style.height = "0%";
+ }
+
+ function GameStartCountdown(){
+ 	var countDownDate = new Date().getTime() + 3510;
+
+ 	var x = setInterval(function(){
 		// Get todays date and time
 		var now = new Date().getTime();	
 		// Find the distance between now an the count down date
@@ -89,14 +81,56 @@ function openNav() {
 		document.getElementById("startCountdown").innerHTML = seconds + "s ";
 	},100);
 
-	var y = setTimeout(function(){
-		console.log("Clear Interval")
-		clearInterval(x);
-		closeNav(x,y);
-	},3000);
-}
+ 	var y = setTimeout(function(){
+ 		console.log("Clear Interval")
+ 		clearInterval(x);
+ 		closeNav(x,y);
+ 	},3000);
+ }
 
-function closeNav() {
-    startRound();
-    document.getElementById("myNav").style.height = "0%";
+function toggleSignIn() {
+	document.getElementById('signin').style.display = "none";
+	document.getElementById('countdown').style.display = 'block';
+	user = firebase.auth().currentUser;
+    if (user) {
+    	var name = document.getElementById("name").value;
+        user.updateProfile({
+        	displayName: name
+        }).then(function() {
+        	//success
+        }, function(error){
+        	//fail
+        	console.log(error);
+        });
+    } else {
+        firebase.auth().signInAnonymously().catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          if (errorCode === 'auth/operation-not-allowed') {
+            alert('You must enable Anonymous auth in the Firebase Console.');
+          } else {
+            console.log(user.displayName);
+          }
+        });
+        console.log("Signed In");
+        toggleSignIn();
+    }
+    GameStartCountdown();
+}
+function initApp() {
+      // Listening for auth state changes.
+      // [START authstatelistener]
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          console.log(user);
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          console.log("Signed In - Init");
+        } else {
+          // User is signed out.
+          console.log("Signed Out - Init");
+        }
+    });
 }
